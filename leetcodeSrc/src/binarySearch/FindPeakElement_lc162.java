8d3a3bfbc93983d95e0f821d94a68b0c09718722
ChrisLee <337053797@qq.com>
2024-04-09 23:24:04 +0800

    3.09 leetcode

---
+++ leetcodeSrc/src/binarySearch/FindPeakElement_lc162.java
@@ -0,0 +1,35 @@
+package binarySearch;
+
+public class FindPeakElement_lc162 {
+
+    class Solution {
+        public int findPeakElement(int[] nums) {
+            int length = nums.length;
+            // 先检查两端
+            if (length == 1) {
+                return 0;
+            }
+            if (nums[0] > nums[1]) {
+                return 0;
+            }
+            if (nums[length - 1] > nums[length - 2]) {
+                return length - 1;
+            }
+            int l = 1, r = length - 2, m, ans = -1;
+            // 要牢记两端都是无穷小，这样中间必有峰值
+            while (l <= r) {
+                m = l + (r - l) / 2;
+                if (nums[m] < nums[m - 1]) {
+                    r = m - 1;
+                } else if (nums[m] < nums[m + 1]) {
+                    l = m + 1;
+                } else {
+                    // 这个else分支就是 m-1<m<m+1 的情况，m就是峰值
+                    ans = m;
+                    break;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcodeSrc/src/linkedListStructure/AddTwoNumbers_lc002.java
@@ -0,0 +1,34 @@
+package linkedListStructure;
+
+public class AddTwoNumbers_lc002 {
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+            // 新建节点连在后面
+            ListNode ans = null, cur = null;
+            int carry = 0;
+            // for循环里面注意中间的条件式满足才执行，别写反了
+            for (int sum, val;
+                 l1 != null || l2 != null;
+                 l1 = l1 == null ? null : l1.next,
+                         l2 = l2 == null ? null : l2.next) {
+
+                sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+                val = sum % 10;
+                carry = sum / 10;
+                if (ans == null) {
+                    ans = new ListNode(val);
+                    cur = ans;
+                } else {
+                    cur.next = new ListNode(val);
+                    // 这里注意cur要后移
+                    cur = cur.next;
+                }
+            }
+            if (carry == 1) {
+                cur.next = new ListNode(1);
+            }
+            return ans;
+        }
+    }
+}
